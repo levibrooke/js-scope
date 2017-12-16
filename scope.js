776e6096bd2b1bc1ff5939dc136f154ad3d5c6a7
levi <levi@levibrooke.com>
2017-12-16 02:04:35 -1000

    Complete exercises

--- scope.js
+++ scope.js
@@ -3,10 +3,12 @@ var isCookieJarOpen = null;
 
 function closeLid() {
   /* answer here */
+  isCookieJarOpen = false;
 }
 
 function openLid() {
   /* answer here */
+  isCookieJarOpen = true;
 }
 
 
@@ -17,7 +19,7 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return /* answer here */;
+    return hello + " " + world;
   }
   return innerFunction();
 }
@@ -31,8 +33,8 @@ function addMatrixElements(matrix) {
 
   for(var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var i = 0; i < matrix[i].length; i++) {
-      result = result + matrix[i][i];
+    for(var j = 0; j < matrix[i].length; j++) {
+      result = result + matrix[i][j];
     }
   }
   return result;
@@ -45,8 +47,8 @@ function sendDataToClient() {
 
   var userObject = {
     handle: 'neo',
-    authenticated: false
-  }
+    authenticated: true
+  };
 
   function authenticateUser(obj, username) {
     var userObject = {
@@ -56,11 +58,11 @@ function sendDataToClient() {
 
     if (userObject.handle === username) {
       userObject.authenticated = true;
-      return userObject
+      return userObject;
     } else {
-      return userObject
+      return userObject;
     }
   }
-  authenticateUser(userObject, 'neo')
-  return userObject
+  authenticateUser(userObject, 'neo');
+  return userObject;
 }
